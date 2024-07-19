1ac982ba82d558b567f709c74c3705f8efd107a3
BirushaNdegeya <birushandegeya@gmail.com>
2024-07-19 10:47:10 +0200

    add auth require signin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -73,7 +73,7 @@ export default class AuthController {
    static requireSignin = expressjwt({
       secret: mainConfig.jwtSecret,
       algorithms: ["HS256"]
-   },);
+   });
 
    static hasAuthorization(
       req: Request | any,
